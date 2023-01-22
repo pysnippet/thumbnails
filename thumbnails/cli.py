354e69f6329548a35742456688adf2e7f03e9bf7
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-01-22 18:57:00 +0400

    Gh-26: Move the help messages to constants

--- thumbnails/cli.py
+++ thumbnails/cli.py
@@ -5,6 +5,7 @@ import click
 from . import ThumbnailFactory
 from . import __version__
 
+# Default values of the particular option of the CLI.
 DEFAULT_BASE = ""
 DEFAULT_SKIP = False
 DEFAULT_OUTPUT = "."
@@ -12,18 +13,26 @@ DEFAULT_FORMAT = "vtt"
 DEFAULT_COMPRESS = 1.0
 DEFAULT_INTERVAL = 1.0
 
-# This defines a set of supported values for the particular option of the CLI.
-_type = click.Choice(ThumbnailFactory.thumbnails.keys(), case_sensitive=False)
+# Help messages of the particular option of the CLI.
+HELP_BASE = "The prefix of the thumbnails path can be customized."
+HELP_SKIP = "Skip the existing thumbnails. Default is not set."
+HELP_OUTPUT = "The output directory. Default is the current directory."
+HELP_FORMAT = "Output format. Default is %s." % DEFAULT_FORMAT
+HELP_COMPRESS = "The image scale coefficient. A number from 0 to 1."
+HELP_INTERVAL = "The interval between neighbor thumbnails in seconds."
+
+# This defines a choice of supported values for the '--format' option of the CLI.
+format_choice = click.Choice(ThumbnailFactory.thumbnails.keys(), case_sensitive=False)
 
 
 def cli(func):
     @click.command()
-    @click.option("--compress", "-C", default=DEFAULT_COMPRESS, help="The image scale coefficient. A number from 0 to 1.")
-    @click.option("--interval", "-I", default=DEFAULT_INTERVAL, help="The interval between neighbor thumbnails in seconds.")
-    @click.option("--base", "-B", default=DEFAULT_BASE, help="The prefix of the thumbnails path can be customized.")
-    @click.option("--skip", "-S", default=DEFAULT_SKIP, help="Skip the existing thumbnails. Default is not set.")
-    @click.option("--output", "-O", default=DEFAULT_OUTPUT, type=click.Path(), help="The output directory. Default is the current directory.")
-    @click.option("--format", "-F", default=DEFAULT_FORMAT, type=_type, help="Output format. Default is %s." % DEFAULT_FORMAT)
+    @click.option("--compress", "-C", default=DEFAULT_COMPRESS, help=HELP_COMPRESS)
+    @click.option("--interval", "-I", default=DEFAULT_INTERVAL, help=HELP_INTERVAL)
+    @click.option("--base", "-B", default=DEFAULT_BASE, help=HELP_BASE)
+    @click.option("--skip", "-S", default=DEFAULT_SKIP, help=HELP_SKIP)
+    @click.option("--output", "-O", default=DEFAULT_OUTPUT, type=click.Path(), help=HELP_OUTPUT)
+    @click.option("--format", "-F", default=DEFAULT_FORMAT, type=format_choice, help=HELP_FORMAT)
     @click.argument("inputs", required=True, type=click.Path(), nargs=-1)
     @click.version_option(__version__, "-V", "--version")
     @click.help_option("-h", "--help")
