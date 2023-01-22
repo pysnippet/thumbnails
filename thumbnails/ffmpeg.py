a55ba45a5a4a7bb2d74dc9b1dd28d351169e0204
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-01-22 22:39:36 +0400

    Fix Codacy #B603 issue without breaking behaviour

--- thumbnails/ffmpeg.py
+++ thumbnails/ffmpeg.py
@@ -41,6 +41,7 @@ class _FFMpeg:
 
             process = subprocess.Popen(
                 (ffmpeg_bin, "-hide_banner", "-i", filename),
+                shell=False,
                 bufsize=100000,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
