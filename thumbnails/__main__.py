e85a8dab579e8b3cf8e1c173d371be00494889cb
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-01-22 14:35:36 +0400

    Gh-26: Add input validation and check for existence

--- thumbnails/__main__.py
+++ thumbnails/__main__.py
@@ -23,27 +23,15 @@ def worker(video, format_):
     formatter.generate()
 
 
-class _ThumbnailsCLI(click.Command):
-    """This class overrides the usages section of the help message."""
-
-    def format_usage(self, ctx, formatter):
-        usages = (
-            "[OPTIONS] INPUT_DIR",
-            "[OPTIONS] INPUT_FILE",
-            "[OPTIONS] INPUT_FILES...",
-        )
-        formatter.write_usage(ctx.command_path, "\n\t\t\t".join(usages), prefix="Usages: ")
-
-
 # This defines a set of supported values for the particular option of the CLI.
 _type = click.Choice(FormatterFactory.thumbnails.keys(), case_sensitive=False)
 
 
-@click.command(cls=_ThumbnailsCLI)
+@click.command()
 @click.option("--compress", "-C", default=DEFAULT_COMPRESS, help="The image scale coefficient. A number from 0 to 1.")
 @click.option("--interval", "-I", default=DEFAULT_INTERVAL, help="The interval between neighbor thumbnails in seconds.")
 @click.option("--base", "-B", default=DEFAULT_BASE, help="The prefix of the thumbnails path can be customized.")
-@click.option("--skip", "-S", default=DEFAULT_SKIP, help="Skip the existing thumbnails. Default is False.")
+@click.option("--skip", "-S", default=DEFAULT_SKIP, help="Skip the existing thumbnails. Default is not set.")
 @click.option("--output", "-O", default=DEFAULT_OUTPUT, type=click.Path(), help="The output directory. Default is the current directory.")
 @click.option("--format", "-F", default=DEFAULT_FORMAT, type=_type, help="Output format. Default is %s." % DEFAULT_FORMAT)
 @click.argument("inputs", required=True, type=click.Path(), nargs=-1)
@@ -51,8 +39,19 @@ _type = click.Choice(FormatterFactory.thumbnails.keys(), case_sensitive=False)
 @click.help_option("-h", "--help")
 def thumbnails_cli(compress, interval, base, inputs, output, skip, **kwargs):
     """TODO: This section will be completed after fixing the issue #26."""
+    if not any([
+        all(map(os.path.isfile, inputs)),
+        all(map(os.path.isdir, inputs)),
+    ]):
+        exit("The inputs must be all files or all directories.")
+
+    if not skip and any(map(os.path.exists, inputs)):
+        skip = click.confirm("Are you agree to override?", default=False)
+
+    print(skip)
+    return
+
     format_ = kwargs.pop("format")
-    output_is_directory = all((len(inputs) > 1, *map(os.path.isfile, inputs))) or os.path.isdir(inputs[0])
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         videos = executor.map(
