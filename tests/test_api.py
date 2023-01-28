65cbf1523883b63aee737c6863327f8c523101d8
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-01-28 21:43:44 +0400

    Fix the CI fail issue

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,19 +9,22 @@ def thumbnail_generation_with_default_output(tmp_media, inputs, fmt):
     generator.format = fmt
     generator.generate()
 
-    with (
-        open("%s/snapshots/relative-base-%s" % (tmp_media, fmt)) as snapshot,
-        open("%s/avi/video.%s" % (tmp_media, fmt)) as avi_result,
-        open("%s/ogv/video.%s" % (tmp_media, fmt)) as ogv_result,
-    ):
-        snapshot = snapshot.read()
-        avi_snapshot = snapshot % {"tmp_media": os.path.join(os.path.relpath(tmp_media), "avi")}
-        ogv_snapshot = snapshot % {"tmp_media": os.path.join(os.path.relpath(tmp_media), "ogv")}
-        if fmt == "json":
-            assert len(os.listdir("%s/avi/video" % tmp_media)) == 11
-            assert len(os.listdir("%s/ogv/video" % tmp_media)) == 11
-        assert avi_snapshot == avi_result.read()
-        assert ogv_snapshot == ogv_result.read()
+    snapshot = open("%s/snapshots/relative-base-%s" % (tmp_media, fmt))
+    avi_result = open("%s/avi/video.%s" % (tmp_media, fmt))
+    ogv_result = open("%s/ogv/video.%s" % (tmp_media, fmt))
+
+    snapshot_data = snapshot.read()
+    avi_snapshot = snapshot_data % {"tmp_media": os.path.join(os.path.relpath(tmp_media), "avi")}
+    ogv_snapshot = snapshot_data % {"tmp_media": os.path.join(os.path.relpath(tmp_media), "ogv")}
+    if fmt == "json":
+        assert len(os.listdir("%s/avi/video" % tmp_media)) == 11
+        assert len(os.listdir("%s/ogv/video" % tmp_media)) == 11
+    assert avi_snapshot == avi_result.read()
+    assert ogv_snapshot == ogv_result.read()
+
+    snapshot.close()
+    avi_result.close()
+    ogv_result.close()
 
 
 def thumbnail_generation_with_with_extras(tmp_media, inputs, fmt):
@@ -33,13 +36,15 @@ def thumbnail_generation_with_with_extras(tmp_media, inputs, fmt):
     generator.interval = 10
     generator.generate()
 
-    with (
-        open("%s/snapshots/specified-base-%s" % (tmp_media, fmt)) as snapshot,
-        open("%s/thumbnails/video.%s" % (tmp_media, fmt)) as result,
-    ):
-        if fmt == "json":
-            assert len(os.listdir("%s/thumbnails/video" % tmp_media)) == 11
-        assert snapshot.read() == result.read()
+    snapshot = open("%s/snapshots/specified-base-%s" % (tmp_media, fmt))
+    result = open("%s/thumbnails/video.%s" % (tmp_media, fmt))
+
+    if fmt == "json":
+        assert len(os.listdir("%s/thumbnails/video" % tmp_media)) == 11
+    assert snapshot.read() == result.read()
+
+    snapshot.close()
+    result.close()
 
 
 def test_api_vtt_generation_directory_inputs_default_output(tmp_media):
